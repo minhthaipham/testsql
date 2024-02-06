6698d501ee00a164279e41ceb8239fde1d0c8ee9
minhthai <minhthai23012002@gmail.com>
2024-02-06 15:42:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require("rootpath")();
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/users", require("./users/users.controller"));
+
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
+app.listen(port, () => console.log("Server listening on port " + port));
